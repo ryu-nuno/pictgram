90c4c6eb6dc3e42ea32e09ad26fcb76fb651afdc
nunoo <shion9bsk@gmail.com>
2021-02-07 11:58:03 +0000

    third commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
     
-    validates :name, presence: true
-    validates :email, presence: true
-    validates :password, length: {minimum: 8 }
-    validates :password, length: {maximum: 32 }
-    validates :password,presence:true
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    VALID_PASSWORD_REGEX =/\A[a-z0-9]+\z/i
     
+    validates :name, presence: true,
+              length:{maximum: 15}
+    validates :email, presence: true,
+               format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    
+    validates :password, length: {minimum: 8,maximum: 32},
+               format: { with: VALID_PASSWORD_REGEX}
     has_secure_password
 end
