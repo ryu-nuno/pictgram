d3a532108784463f4e50665caedfaae26f5964df
nunoo <shion9bsk@gmail.com>
2021-02-08 23:31:20 +0000

    third commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    VALID_PASSWORD_REGEX =/\A[a-z0-9]+\z/i
+    VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i
     
     validates :name, presence: true,
               length:{maximum: 15}
