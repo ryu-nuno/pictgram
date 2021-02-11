3604166a2a8de067e95758cf6fa1415f0d83bec3
nunoo <shion9bsk@gmail.com>
2021-02-11 00:04:44 +0000

    forth commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(email: params[:session][:email])
+    
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
@@ -19,18 +20,20 @@ class SessionsController < ApplicationController
   end
   
   private
-  def log_in(user)
-    session[:user_id] = user.id
-  end
-  
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
-  end
-  
-  private
-  def user_params
-    params.require(:user).permit(:name, :email,:password, :password_confirmation)
-  end
-  
+    def email_params
+      params.require(:session).permit(:email)
+    end
+
+    def password_params
+      params.require(:session).permit(:password)
+    end
+
+    def log_in(user)
+      session[:user_id] = user.id
+    end
+
+    def log_out
+      session.delete(:user_id)
+      @current_user = nil
+    end
 end
